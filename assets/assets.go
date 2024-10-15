104114f389e163850637d5aa78f9eb2ca3f07cbd
Wendell Beckwith <wendell.beckwith@redfin.com>
2024-10-15 08:31:40 -0500

    Refactored assets directory use

---
+++ assets/assets.go
@@ -0,0 +1,38 @@
+package assets
+
+import (
+	"embed"
+	corev1 "k8s.io/api/core/v1"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+)
+
+var (
+	//go:embed manifests
+	manifests embed.FS
+
+	assetsScheme = runtime.NewScheme()
+	assetsCodecs = serializer.NewCodecFactory(assetsScheme)
+)
+
+func init() {
+	utilruntime.Must(corev1.AddToScheme(assetsScheme))
+	utilruntime.Must(appsv1.AddToScheme(assetsScheme))
+}
+
+func GetPersistentVolumeClaimFromFile(name string) (*corev1.PersistentVolumeClaim, error) {
+	pvcDataBytes, err := manifests.ReadFile(name)
+	if err != nil {
+		return nil, err
+	}
+	pvcDataObject, err := runtime.Decode(assetsCodecs.UniversalDecoder(corev1.SchemeGroupVersion), pvcDataBytes)
+	if err != nil {
+		return nil, err
+	}
+	pvcData := pvcDataObject.(*corev1.PersistentVolumeClaim)
+	return pvcData, nil
+
+}
